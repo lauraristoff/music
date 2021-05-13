f11cad93dd2c8b8c2722ecbc56c2fa800d8272c6
lauraristoff <lauraristoff83@gmail.com>
2021-05-13 12:34:29 +0000

    update all fies. Add css animations and js for interactivity

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+/*
+Author: Laura Ristoff
+File Name: script.js
+Date: 05/13/2021
+*/
+
+//Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        }
+}
